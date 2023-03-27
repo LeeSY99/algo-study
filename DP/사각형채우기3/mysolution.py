341638258177e78a800a10dceb2de525322e4d21
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2023-03-27 21:47:13 +0900

    Add files via upload

---
+++ DP/사각형채우기3/mysolution.py
@@ -0,0 +1,13 @@
+n=int(input())
+
+dp=[0]*(n+1)
+
+dp[0]=1
+dp[1]=2
+
+for i in range(2,n+1):
+    dp[i]=(2 * dp[i-1] + 3 * dp[i-2]) % 1000000007
+    for j in range(i-3,-1,-1):
+        dp[i]=(dp[i]+ 2*dp[j]) % 1000000007
+
+print(dp[n])
