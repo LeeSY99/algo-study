56dee4a6ae4cf294f9c0470ca8b7500e9d01cae1
Sangyoun Lee <101381138+LeeSY99@users.noreply.github.com>
2023-03-29 21:09:58 +0900

    Update mysolution.py

--- DP/사각형채우기3/mysolution.py
+++ DP/사각형채우기3/mysolution.py
@@ -7,7 +7,7 @@ dp[1]=2
 
 for i in range(2,n+1):
     dp[i]=(2 * dp[i-1] + 3 * dp[i-2]) % 1000000007
-    for j in range(i-3,-1,-1):
+    for j in range(i-3,-1,-1): #i-3~ 0까지 항상 2개씩 존재
         dp[i]=(dp[i]+ 2*dp[j]) % 1000000007
 
 print(dp[n])
